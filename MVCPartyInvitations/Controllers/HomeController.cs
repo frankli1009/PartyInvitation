5a0c34c4a0cfed846a81d27809d4754a63021799
frankli1009 <frankli1009@gmail.com>
2017-08-09 16:27:59 +0100

    Add files via upload

---
+++ MVCPartyInvitations/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCPartyInvitations.Models;
+
+namespace MVCPartyInvitations.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                // ToDo - email response to party organiser
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVCPartyInvitations/Models/GuestResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCPartyInvitations.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage="Please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your Email address")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone number")]
+        [RegularExpression("[0-9]{5}-[0-9]{6}", ErrorMessage = "Please enter a valid phone number")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please say whether you're coming")]
+        public bool? WillAttend { get; set; }
+        public string Password { get; set; }
+    }
+}
